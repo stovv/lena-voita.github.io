c815e63f6796452c9dd6c6748f95559fcc9b4c4f
Данила Ставров <d.stavrov@crpo.su>
2022-09-03 14:22:15 +0300

    Declare react-awesome-lightbox

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,27 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module 'react-awesome-lightbox' {
+    import React from 'react';
+    type Slide = {
+        url: string;
+        title: string;
+    };
+
+    const Lightbox: React.FC<{
+        images: Slide[];
+        title?: string;
+        zoomStep?: number;
+        allowZoom?: boolean;
+        startIndex?: number;
+        showTitle?: boolean;
+        onClose?: () => void;
+        allowReset?: boolean;
+        allowRotate?: boolean;
+        doubleClickZoom?: number;
+        onNavigateImage?: (newIndex: number) => void;
+        buttonAlign?: 'flex-end' | 'flex-start' | 'center';
+    }>;
+    export = Lightbox;
+}
